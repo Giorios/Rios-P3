6ffd7b90d53e5d12808bbcea69c774dedcbbafc7
Giorios <90624324+Giorios@users.noreply.github.com>
2024-03-20 09:45:28 -0400

    steps 1-4 finished

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+         title: "IMDB + Gio's Top 8 Movies",
+         owner: 'Giovanni',
+         github: 'https://github.com/Giorios/Rios-P3'
       }
     },
       methods: {
