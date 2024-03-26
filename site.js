ce1c72d8b299b985710edfeb9c41e29c6404fcb2
Giorios <90624324+Giorios@users.noreply.github.com>
2024-03-26 11:00:22 -0400

    working on step 7

--- site.js
+++ site.js
@@ -41,12 +41,48 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            getMonthText(dateArray) {
-                  switch(movie.released){
-                        case 0:
-                        text = "October 14, 1994"
+           getMonthText(dateArray) {
+                  switch(dateArray[1]){
+                        case 1:
+                        text = "January"
                         break;
+                        case 2:
+                        text = "February"
+                        break;
+                        case 3:
+                        text = "March"
+                        break;
+                        case 4:
+                        text = "April"
+                        break;
+                        case 5:
+                        text = "May"
+                        break;
+                        case 6:
+                        text = "June"
+                        break;
+                        case 7:
+                        text = "July"
+                        break;
+                        case 8:
+                        text = "August"
+                        break;
+                        case 9:
+                        text = "September"
+                        break;
+                        case 10:
+                        text = "October"
+                        break;
+                        case 11:
+                        text = "November"
+                        break;
+                        case 12:
+                        text = "December"
+                        break;
+
                   }
+
+                  return text//month word DAY, Year
             }
       }
 })
